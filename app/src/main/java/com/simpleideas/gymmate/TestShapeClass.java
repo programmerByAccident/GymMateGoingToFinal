18192afbd6e077d1bacffb3e53f8c6d6b2172ebb
programmerByAccident <ciopeigalex@gmail.com>
2017-03-06 20:08:19 +0200

    added last drawable files to git

---
+++ app/src/main/java/com/simpleideas/gymmate/TestShapeClass.java
@@ -0,0 +1,42 @@
+package com.simpleideas.gymmate;
+
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.shapes.Shape;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+
+/**
+ * Created by Geprge on 3/6/2017.
+ */
+
+    public class TestShapeClass extends Shape {
+
+        private static final float WIDTH = 12f;
+        private int[] colors;
+
+        public TestShapeClass(int[] colors){
+            this.colors = colors;
+        }
+
+        @Override
+        public void draw(Canvas canvas, Paint paint) {
+
+            float cx = getWidth() / 2;
+            float cy = getHeight() / 2;
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(10);
+            float radius = Math.min(cx, cy) - WIDTH/2;
+
+            for(int color:colors){
+                paint.setColor(color);
+                canvas.drawCircle(cx,cy,radius,paint);
+                radius-=WIDTH/2;
+            }
+
+        }
+    }
+
+
+
